0f08c495e98bfc50a6a693dad3fce7758de26634
BenHwaet <benmdesroches@gmail.com>
2023-07-22 20:05:42 -0300

    added the questions to be posed in the command line - using inquirer package - and confirmed that they are answerable as well. However, the generateREADME function is not being recognized as a function, so no fule is being written yet.

--- index.js
+++ index.js
@@ -1,36 +1,63 @@
 // Include packages needed for this application 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
 
-
+const generateREADME = require('./utils/generateMarkdown.js');
+console.log(generateREADME);
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer
+    .prompt([
     {
-    type: 'input',
-    name: 'title',
-    message: 'What is your project\'s title?'
+     type: 'input',
+     name: 'title',
+     message: 'What is your project\'s title?'
     },
     {
-        
+     type: 'input',
+     name: 'description',
+     message: 'Please describe your project, including your motivation, what it solves, and what your learned.',
+    },
+    {
+     type: 'checkbox',
+     name: 'tableOfContents',
+     message: 'Which sections do you wish to include in the table of contents, if any?',
+     choices: ['Installation', 'Usage', 'Credits', 'License']   
+    },
+    {
+     type: 'input',
+     name: 'installation',
+     message: 'What steps must be followed to install your project?'   
+    },
+    {
+     type: 'input',
+     name: 'usage',
+     message:'Please provide instructions '    
+    },
+    {
+    //License list taken from <https://choosealicense.com/licenses/>
+     type: 'list',
+     name: 'license',
+     message: 'Which license would you like to use?',
+     choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License', 
+     'Apache License 2.0', 'MIT License', 'Boost Software License 1.0',
+    'The Unlicense']   
     }
-
-
-
-];
+])
+.then((answers) => {
+    const READMEContent = generateREADME(answers);
 
 // TODO: Create a function to write README file
-function writeToFile('genREADME.md', data) {
-    
-}
+fs.writeFile('README.md', READMEContent, (err) => 
+    err ? console.log(err) : console.log('Success!'))
+})
 
 // Function to initialize app - taken from NodeJS mini-project code
-const init = () => {
-    promptUser()
-    .then((answers) => writeFile('genREADME.md', generateMarkdown(answers)))
-    .then (() => console.log('Success!'))
-    .catch((err) => console.error(err));
-};
+// const init = () => {
+//     promptUser()
+//     .then((answers) => writeFile('genREADME.md', generateMarkdown(answers)))
+//     .then (() => console.log('Success!'))
+//     .catch((err) => console.error(err));
+// };
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+const sections = ['title', 'installation', 'usage', 'image', 'credits', 'license', 
+'badgeUrl', 'features', 'contribute', 'tests'];
+
+const allSections = [...sections]
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateREADME = (allSections) =>
+ `# ${title}
 
   ## Description
-  ${data.description}
+  ${description}
   
   ## Table of Content
   
@@ -26,38 +31,38 @@ function generateMarkdown(data) {
   
   ## Installation
   
-  ${data.installation}
+  ${installation}
   
   ## Usage
 
-  ${data.usage}
+  ${usage}
 
-  ${data.image}
+  ${image}
 
   ## Credits
 
-  ${data.credits}
+  ${credits}
 
   ## License
 
-  ${data.license}
+  ${license}
 
   ## Badges
 
-  ![image](${data.BadgeURL})
+  ![image](${badgeUrl})
 
   ## Features
 
-  ${data.features}
+  ${features}
 
   ## Contribute
 
-  ${data.contribute}
+  ${contribute}
 
   ## Tests
 
-  ${data.tests}
+  ${tests}
 `;
-}
+console.log(allSections);
 
-module.exports = generateMarkdown;
+module.exports = { generateREADME };
